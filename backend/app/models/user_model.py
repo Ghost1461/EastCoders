ae72ec86fff433e9cab3e1380958b60421e79698
aylashamsy <ayla.shmsy82@gmail.com>
2026-05-13 21:03:28 +0330

    User authentication done

---
+++ backend/app/models/user_model.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from datetime import datetime
+
+from app.core.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    full_name = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    phone_number = Column(String, nullable=False)
+
+    hashed_password = Column(String, nullable=False)
+
+    role = Column(String, default="seller")
+
+    created_at = Column(DateTime, default=datetime.utcnow)
