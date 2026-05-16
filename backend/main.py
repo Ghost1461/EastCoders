1cb180f66afd2b1469eb1be41a44027507e89531
aylashamsy <ayla.shmsy82@gmail.com>
2026-05-16 22:59:35 +0330

    last version of notifications and scheduled main

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from apscheduler.schedulers.background import BackgroundScheduler
+
 from app.routers import router
-from app.core.database import Base, engine
+from app.core.database import (
+    Base,
+    engine,
+    SessionLocal
+)
+
 from app import models
+
+from app.services.news_fetch_service import NewsFetchService
+
 
 app = FastAPI(
     title="EastCoders API",
@@ -11,6 +21,49 @@ app = FastAPI(
 app.include_router(router)
 
 Base.metadata.create_all(bind=engine)
+
+
+scheduler = BackgroundScheduler()
+
+
+def fetch_news_job():
+    db = SessionLocal()
+
+    try:
+        service = NewsFetchService()
+
+        service.fetch_and_store_news(
+            db=db,
+            category="fashion"
+        )
+
+        service.fetch_and_store_news(
+            db=db,
+            category="commerce_finance"
+        )
+
+        print("AUTO NEWS FETCH COMPLETED")
+
+    except Exception as e:
+        print("AUTO NEWS FETCH ERROR:", e)
+
+    finally:
+        db.close()
+
+
+@app.on_event("startup")
+def start_scheduler():
+
+    scheduler.add_job(
+        fetch_news_job,
+        "interval",
+        hours=3
+    )
+
+    scheduler.start()
+
+    print("NEWS SCHEDULER STARTED")
+
 
 @app.get("/")
 def home():
